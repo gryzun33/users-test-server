dc7fdc36a3e10e9618658849daa7000fcab38b70
gryzun33 <yakushevaolga564@gmail.com>
2025-03-26 14:09:24 +0300

    feat: add file .env and port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT ?? 4000;
+  await app.listen(PORT);
+  console.log(`Application is running on port ${PORT}`);
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  console.error('Error during application startup:', error);
+  process.exit(1);
+});
