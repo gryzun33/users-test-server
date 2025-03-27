02084cb9957f437859becc3480bcc0e721912211
gryzun33 <yakushevaolga564@gmail.com>
2025-03-27 13:06:46 +0300

    refactor: delete appcontroller and appservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module, OnModuleInit } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [UserModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements OnModuleInit {
   onModuleInit() {
