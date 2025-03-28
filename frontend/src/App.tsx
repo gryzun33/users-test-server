0cbb1edc5cb4e992dce5fd3de590dc8983626203
gryzun33 <yakushevaolga564@gmail.com>
2025-03-28 10:45:14 +0300

    feat: add base routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
+import Layout from './components/Layout';
+import UserList from './pages/UserList';
+import CreateUser from './pages/CreateUser';
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/users" />} />
+        <Route element={<Layout />}>
+          <Route path="/users" element={<UserList />} />
+          <Route path="/create" element={<CreateUser />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/UserList.tsx
@@ -0,0 +1,9 @@
+const UserList = () => {
+  return (
+    <div>
+      <h2>UserList</h2>
+    </div>
+  );
+};
+
+export default UserList;
