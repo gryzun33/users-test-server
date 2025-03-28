fe2162fce0d7a6daccac799ffee5b81bd901f0fb
gryzun33 <yakushevaolga564@gmail.com>
2025-03-28 11:57:38 +0300

    feat: add layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,10 +1,50 @@
-import { Outlet } from 'react-router';
+import { Link, Outlet, useLocation } from 'react-router';
+import { Plus } from 'lucide-react';
+import { ArrowLeft } from 'lucide-react';
 
 const Layout = () => {
+  const location = useLocation();
+
+  const isOnUserList = location.pathname === '/users';
+  const isOnCreateUser = location.pathname === '/create';
+
   return (
-    <div>
-      <h1>Layout</h1>
-      <Outlet />
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-slate-700 w-full p-4 ">
+        <div className="flex max-w-7xl mx-auto justify-between items-center">
+          <Link to="/" className="text-2xl font-bold text-red-400">
+            UserBase
+          </Link>
+          <nav>
+            {isOnUserList && (
+              <Link
+                to="/create"
+                className="flex min-w-[185px] gap-2 px-4 py-2 bg-currentColor border-[1px] border-slate-300 text-slate-200 hover:text-white font-medium rounded-lg hover:bg-slate-500 transition-colors"
+              >
+                <Plus />
+                <span> Add New User</span>
+              </Link>
+            )}
+            {isOnCreateUser && (
+              <Link
+                to="/users"
+                className="flex min-w-[185px] gap-2 px-4 py-2 bg-currentColor border-[1px] border-slate-300 text-slate-200 hover:text-white font-medium rounded-lg hover:bg-slate-500 transition-colors"
+              >
+                <ArrowLeft />
+                <span> Back to Userlist</span>
+              </Link>
+            )}
+          </nav>
+        </div>
+      </header>
+      <main className="flex-grow p-4 bg-slate-200">
+        <Outlet />
+      </main>
+      <footer className="bg-slate-200 text-slate-500 text-sm p-4 border-t-[1px] border-slate-300">
+        <div className="flex max-w-7xl mx-auto justify-center items-center">
+          <p>&copy; 2025 UserBase. All rights reserved.</p>
+        </div>
+      </footer>
     </div>
   );
 };
