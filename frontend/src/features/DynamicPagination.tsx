165afb536b3764b58363e8a232acfe1ae2b52056
gryzun33 <yakushevaolga564@gmail.com>
2025-03-29 18:58:01 +0300

    refactor: refactor pagination

--- frontend/src/features/DynamicPagination.tsx
+++ frontend/src/features/DynamicPagination.tsx
@@ -9,93 +9,77 @@ import {
 } from '@/components/ui/pagination';
 import { useDispatch } from 'react-redux';
 import { setPage } from '@/store/slices/paginationSlice';
-import { MAX_PAGES_SHOW, USERS_PER_PAGE } from '@/utils/constants';
+import { USERS_PER_PAGE } from '@/utils/constants';
 import { memo } from 'react';
 import { useNavigate } from 'react-router';
+import { usePagination } from '@/hooks/usePagination';
 
-const DynamicPagination = memo(
-  ({
+type Props = {
+  totalPages: number;
+  currentPage: number;
+};
+
+const DynamicPagination = memo(({ totalPages, currentPage }: Props) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { pages, leftEllipsis, rightEllipsis } = usePagination(
     totalPages,
-    currentPage,
-  }: {
-    totalPages: number;
-    currentPage: number;
-  }) => {
-    const navigate = useNavigate();
+    currentPage
+  );
 
-    const dispatch = useDispatch();
+  const handlePageChange = (page: number) => {
+    dispatch(setPage(page));
+    navigate(`?page=${page}&limit=${USERS_PER_PAGE}`);
+  };
 
-    const handlePageChange = (page: number) => {
-      dispatch(setPage(page));
-      navigate(`?page=${page}&limit=${USERS_PER_PAGE}`);
-    };
+  return (
+    <Pagination className="mb-0 mt-auto">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          />
+        </PaginationItem>
 
-    const pages = [];
-
-    let startPage = Math.max(currentPage - 1, 1);
-    let endPage = Math.min(currentPage + 1, totalPages);
-
-    if (currentPage <= 2) {
-      endPage = Math.min(MAX_PAGES_SHOW, totalPages);
-    } else if (currentPage >= totalPages - 1) {
-      startPage = Math.max(totalPages - MAX_PAGES_SHOW + 1, 1);
-    }
-
-    for (let i = startPage; i <= endPage; i++) {
-      pages.push(i);
-    }
-
-    const leftEllipsis = startPage > 1;
-    const rightEllipsis = endPage < totalPages;
-
-    return (
-      <Pagination className="mb-0 mt-auto">
-        <PaginationContent>
+        {leftEllipsis && (
           <PaginationItem>
-            <PaginationPrevious
-              onClick={() => handlePageChange(currentPage - 1)}
-              disabled={currentPage === 1}
-            />
+            <PaginationEllipsis />
           </PaginationItem>
+        )}
 
-          {leftEllipsis && (
-            <PaginationItem>
-              <PaginationEllipsis />
+        {pages.map((page) =>
+          currentPage === page ? (
+            <PaginationItem key={page}>
+              <PaginationLink isActive onClick={() => handlePageChange(page)}>
+                {page}
+              </PaginationLink>
             </PaginationItem>
-          )}
-
-          {pages.map((page) =>
-            currentPage === page ? (
-              <PaginationItem key={page}>
-                <PaginationLink isActive onClick={() => handlePageChange(page)}>
-                  {page}
-                </PaginationLink>
-              </PaginationItem>
-            ) : (
-              <PaginationItem key={page}>
-                <PaginationLink onClick={() => handlePageChange(page)}>
-                  {page}
-                </PaginationLink>
-              </PaginationItem>
-            )
-          )}
-
-          {rightEllipsis && (
-            <PaginationItem>
-              <PaginationEllipsis />
+          ) : (
+            <PaginationItem key={page}>
+              <PaginationLink onClick={() => handlePageChange(page)}>
+                {page}
+              </PaginationLink>
             </PaginationItem>
-          )}
+          )
+        )}
 
+        {rightEllipsis && (
           <PaginationItem>
-            <PaginationNext
-              onClick={() => handlePageChange(currentPage + 1)}
-              disabled={currentPage === totalPages}
-            />
+            <PaginationEllipsis />
           </PaginationItem>
-        </PaginationContent>
-      </Pagination>
-    );
-  }
-);
+        )}
+
+        <PaginationItem>
+          <PaginationNext
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+});
 
 export default DynamicPagination;
---
+++ frontend/src/hooks/usePagination.tsx
@@ -0,0 +1,31 @@
+import { MAX_PAGES_SHOW } from '@/utils/constants';
+import { useMemo } from 'react';
+
+export const usePagination = (totalPages: number, currentPage: number) => {
+  const pages = useMemo(() => {
+    const result: number[] = [];
+    let startPage = Math.max(currentPage - 1, 1);
+    let endPage = Math.min(currentPage + 1, totalPages);
+
+    if (currentPage <= 2) {
+      endPage = Math.min(MAX_PAGES_SHOW, totalPages);
+    } else if (currentPage >= totalPages - 1) {
+      startPage = Math.max(totalPages - MAX_PAGES_SHOW + 1, 1);
+    }
+
+    for (let i = startPage; i <= endPage; i++) {
+      result.push(i);
+    }
+
+    return result;
+  }, [currentPage, totalPages]);
+
+  const leftEllipsis = currentPage > 2;
+  const rightEllipsis = currentPage < totalPages - 1;
+
+  return {
+    pages,
+    leftEllipsis,
+    rightEllipsis,
+  };
+};
