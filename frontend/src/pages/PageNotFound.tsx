1e7d20ffb72026a6e7c5c3adae1eb041f2ca4c9a
gryzun33 <yakushevaolga564@gmail.com>
2025-04-01 12:39:12 +0300

    feat: add styles to pagenotfound

--- frontend/src/pages/PageNotFound.tsx
+++ frontend/src/pages/PageNotFound.tsx
@@ -1,7 +1,8 @@
 const PageNotFound = () => {
   return (
-    <div>
-      <h2>PageNotFound</h2>
+    <div className="w-full h-full flex flex-col justify-center items-center text-slate-400 mt-10">
+      <h2 className="text-9xl font-bold">404</h2>
+      <p className="text-3xl font-bold mt-4">Page Not Found</p>
     </div>
   );
 };
