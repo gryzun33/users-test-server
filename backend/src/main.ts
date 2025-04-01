0b6e99ddb047f2df253c6cbb282e67c473269318
gryzun33 <yakushevaolga564@gmail.com>
2025-04-01 16:43:57 +0300

    feat: add enablecors for all

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,11 +12,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:4173'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  app.enableCors();
 
   const loggingService = app.get(LoggingService);
   const httpAdapter = app.get(HttpAdapterHost);
